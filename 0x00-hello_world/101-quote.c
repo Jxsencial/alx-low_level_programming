8f79663dd626d46968935173229cc78ea8220e40
Jxsencial <jessegye999@gmail.com>
2022-09-12 21:40:23 -0700

    8. UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**                                                                         
+ * main - print the string in the put function
+ *
+ * Description: using the main function
+ * this program prints "programming is like building a multilingual puzzle
+ * Return: 0
+ */
+int main(void)
+{
+	char n[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	fprintf(stderr, "%s\n", n);
+	return (1);
+}
